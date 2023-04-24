e55090bb4e49c21250da408956a285ffccafe7e8
Christian Bermúdez-Rivas <41581329+ChrisBermudezR@users.noreply.github.com>
2023-04-24 18:10:21 -0500

    adición de informe final

--- Analisis_vibrio/Funciones/rasterizar_Variable.R
+++ Analisis_vibrio/Funciones/rasterizar_Variable.R
@@ -1,4 +1,4 @@
- rasterizar_Variable<-function(nombre_variable,longitud, latitud, variable, marea, leyenda){
+ rasterizar_Variable<-function(nombre_variable,longitud, latitud, variable, marea, leyenda, datos){
   assign("WIRE",interpBarnes(longitud, latitud, variable), envir = parent.frame())
   assign(paste0("pts.grid"),expand.grid(Longitud=WIRE$x, Latitud=WIRE$y), envir = parent.frame())
   assign(paste0("pts.grid"), mutate(pts.grid, variable=as.vector(WIRE$zg)), envir = parent.frame())
@@ -11,11 +11,11 @@
     geom_raster(aes(fill = variable))+
     geom_polygon(data=costa, aes(x= long, y= lat, group=group), colour="#3aaa05", fill="#3aaa05") +
     geom_polygon(data=rios, aes(x= long, y= lat, group=group), colour="#bcebfb", fill="#bcebfb") +
-    coord_sf(xlim = c(-78.4055, -78.217), ylim = c(2.55, 2.853), expand = FALSE)+   
+    coord_sf(xlim = c(min(longitud), max(longitud)), ylim = c(min(latitud), max(latitud)), expand = FALSE)+   
     geom_polygon(data=areas_protegidas, aes(x= long, y= lat, group=group), colour="red", fill="transparent") +
     theme_bw()+
-    scale_fill_gradientn(colours = c("#fef0d9", "#fdd49e", "#fdbb84", "#fc8d59", "#ef6548", "#d7301f", "#990000"))+
-    geom_point(data=marea_baja, aes(x= longitud, y= latitud))+
+    scale_fill_gradientn(colours = c("#f0f9e8", "#ccebc5", "#a8ddb5", "#7bccc4", "#4eb3d3", "#2b8cbe", "#08589e"))+
+    #geom_point(data=estaciones, aes(x= longitud, y= latitud))+
     labs(fill=leyenda, title= paste(nombre_variable,"-",marea))
     #ggrepel::geom_text_repel(data=marea_alta,aes(x=longitud, y=latitud,label = Estacion),box.padding   = 0.3, direction = "x")
 }
--- Analisis_vibrio/SupportVectorMachine.R
+++ Analisis_vibrio/SupportVectorMachine.R
@@ -1,33 +1,37 @@
 #######################################################################
-############### SVM 
-pa=read.csv("VibrioTotal.csv")
 library(pROC)
-
-pa<-pa[,5:25]
-
-
 library(caret)
 
-#pa=na.omit(pa)
+source("./Funciones/rasterizar_Variable.R")
+library(raster)
+library(rgdal)
+library(oce)
+library(dplyr)
+############### SVM 
+VibrioData=read.csv("VibrioTotal.csv")
 
-head(pa)
 
-summary(pa)
+VibrioData<-VibrioData[,5:25]
 
 
-head(pa)
+
+
+#VibrioData=na.omit(VibrioData)
+
+summary(VibrioData)
+head(VibrioData)
 
 ############################
 
 
 
-set.seed(5000) #pseudo-repeatability
-trainIndex = createDataPartition(pa$Vibrio, p = .75, 
+set.seed(10000) #pseudo-repeatability
+trainIndex = caret::createDataPartition(VibrioData$Vibrio, p = .75, 
                                  list = FALSE, 
                                  times = 1) #y as basis of splitting
 
-training = pa[ trainIndex,] #75% data for model training
-testing= pa[-trainIndex,] #25% for model testing
+training = VibrioData[ trainIndex,] #75% data for model training
+testing= VibrioData[-trainIndex,] #25% for model testing
 
 training$Vibrio<-as.factor(training$Vibrio)
 testing$Vibrio<-as.factor(testing$Vibrio)
@@ -35,18 +39,12 @@ testing$Vibrio<-as.factor(testing$Vibrio)
 
 
 ## dos altas dos bajas
-training = pa[c(1:12, 15:18,23),]
-
-
-testing= pa[c(13,14,20:24),] #25% for model testing
-
+training = VibrioData[c(1:12, 15:18,23),]
+testing= VibrioData[c(13,14,20:24),] #25% for model testing
 training$Vibrio<-as.factor(training$Vibrio)
 testing$Vibrio<-as.factor(testing$Vibrio)
-
-training = pa[c(1:12, 15:18,23),]
-
-
-testing= pa[c(13,14,20:24),] #25% for model testing
+training = VibrioData[c(1:12, 15:18,23),]
+testing= VibrioData[c(13,14,20:24),] #25% for model testing
 
 
 
@@ -60,14 +58,14 @@ train_control = trainControl(method="cv", number=500)
 
 
 #svm with rbf kernel
-rf_mod_fit=train(Vibrio~ .,
+rf_mod_fit=train(Vibrio~ Densidad + Salinidad,
                data=training,trControl=train_control,method="rf")
 
-svmRadialSigma_mod_fit=train(Vibrio~ Densidad,
+svmRadialSigma_mod_fit=train(Vibrio~ Densidad + Salinidad,
                data=training,trControl=train_control,method="svmRadialSigma")
 
 
-glm_Logit_mod_fit=train(Vibrio~  Densidad,
+glm_Logit_mod_fit=train(Vibrio~  Densidad + Salinidad,
                data=training,trControl=train_control,method="glm", family = "binomial")
 
 
@@ -88,9 +86,6 @@ rf_mod_fit_predict=as.numeric(predict(rf_mod_fit, newdata=testing)) #predict on 
 svmRadialSigma_mod_fit_predict=as.numeric(predict(svmRadialSigma_mod_fit, newdata=testing))
 glm_Logit_mod_fit_predict=as.numeric(predict(glm_Logit_mod_fit, testing))
 #test model fit-auc
-
-glm_Logit_mod_fit$levels
-
 
 rf_matriz_confusion <- confusionMatrix(predict(rf_mod_fit, newdata=testing), testing$Vibrio)
 svmRadialSigma_matriz_confusion <- confusionMatrix(predict(svmRadialSigma_mod_fit, newdata=testing), testing$Vibrio)
@@ -131,7 +126,7 @@ plot.roc(testing[,"Vibrio"],
          print.auc.x=45,)
 
 legend("bottomright", legend=c("Regr. Logistica", "Random Forest", "SVM"), col=c("#a6cee3", "#1f78b4", "#b2df8a"), lwd=4)
-par(pty = "m")
+VibrioDatar(pty = "m")
 
 
 modelosEval_Factor(training$Densidad,"Densidad",Exp_Densidad, training$Vibrio   , "Vibrio")
@@ -142,28 +137,27 @@ dev.off()
 ###############
 
 Datos_Raster=read.csv("Datos_Raster.csv")
-source("./Funciones/rasterizar_Variable.R")
-library(raster)
-library(rgdal)
-library(oce)
-library(dplyr)
+
 costa<-rgdal::readOGR("./SIG_Datos/costa.shp")
 rios<-readOGR("./SIG_Datos/rios_wgs84.shp")
 estaciones<-readOGR("./SIG_Datos/estaciones.shp")
 areas_protegidas<-readOGR("./SIG_Datos/areas_protegidas.shp")
 
-
-#rasterizar_Variable(nombre_variable,longitud, latitud, variable, marea, leyenda)
-
 Datos_Raster$MareaFactor
 Datos_Raster_alta<-  subset(Datos_Raster,MareaFactor == "1")
 Datos_Raster_baja<-  subset(Datos_Raster,MareaFactor == "0")
-rasterizar_Variable("Densidad", Datos_Raster_alta$Longitude, Datos_Raster_alta$Latitude, Datos_Raster_alta$Densidad, "Alta", "Densidad del agua")
+
+source("./Funciones/rasterizar_Variable.R")
+#rasterizar_Variable(nombre_variable,longitud, latitud, variable, marea, leyenda)
+rasterizar_Variable("Salinidad", Datos_Raster_alta$Longitude, Datos_Raster_alta$Latitude, Datos_Raster_alta$Salinidad, "Alta", "Densidad del agua", Datos_Raster)
 rasterizar_Variable("Densidad", Datos_Raster_baja$Longitude, Datos_Raster_baja$Latitude, Datos_Raster_baja$Densidad, "Baja", "Densidad del agua")
 
 
-rasterVar<-list.files(path="./SIG_Datos/grids", pattern = ".tif$", full.names = TRUE)
-rasterVar_names<-list.files(path="./SIG_Datos/grids", pattern = ".tif$", full.names = FALSE)
+
+
+
+rasterVar<-list.files(VibrioDatath="./SIG_Datos/grids", VibrioDatattern = ".tif$", full.names = TRUE)
+rasterVar_names<-list.files(VibrioDatath="./SIG_Datos/grids", VibrioDatattern = ".tif$", full.names = FALSE)
 
 for(Archivos in 1:length(rasterVar_names)) assign(rasterVar_names[Archivos], raster(rasterVar[Archivos]))
 
@@ -173,21 +167,35 @@ plot(Densidad_Baja.tif)
 Densidad_Alta.tif$layer
 denalta<-as.data.frame(Densidad_Alta.tif$layer, xy=TRUE)
 
+colnames(pts.grid)<-c("x","y","Salinidad")
 
-colnames(denalta)<-c("x","y","Densidad")
 
-p1=predict(rf_mod_fit, newdata=denalta)
+cbind(densidadModelo, pts.grid$Salinidad)
+densidadModelo<-cbind(densidadModelo, pts.grid$Salinidad)
+colnames(densidadModelo)<-c("x","y","Densidad","Salinidad")
+p1=predict(glm_Logit_mod_fit, newdata=densidadModelo)
+
+
 
 
 as.data.frame(p1)
-
-denalta$Vibri_glm<-as.data.frame(p1)
-
-denaltaPredi<-cbind(denalta$x,denalta$y,denalta$Vibri_glm)
-
-
-colnames(denaltaPredi)<-c("x","y","Vibri_glm")
-
-
+densidadModelo$Vibri_glm<-as.data.frame(p1)
+denaltaPredi<-cbind(densidadModelo$x,densidadModelo$y,densidadModelo$Vibri_glm)
+colnames(denaltaPredi)<-c("Longitud","Latitud","Vibri_glm")
 rasterPrediccion<-rasterFromXYZ(denaltaPredi)
 plot(rasterPrediccion)
+
+
+
+ggplot(denaltaPredi, aes(Longitud, Latitud)) +
+  geom_raster(aes(fill = Vibri_glm))+
+  geom_polygon(data=costa, aes(x= long, y= lat, group=group), colour="#38a800", fill="#38a800") +
+  #geom_polygon(data=rios, aes(x= long, y= lat, group=group), colour="#bcebfb", fill="#bcebfb") +
+  coord_sf(xlim = c(-78.595484615, -78.053463218), ylim = c(2.339426503 , 3.162306176), expand = FALSE)+   
+  geom_polygon(data=areas_protegidas, aes(x= long, y= lat, group=group), colour="red", fill="transparent", size=1) +
+  theme(panel.background = element_rect(fill = '#bfe8ff', color="#737373"),
+        panel.grid.major = element_line(color = '#252525', linetype = 'dotted'))+
+  scale_fill_gradientn(colours = c("#2166ac", "#67a9cf", "#d1e5f0", "#f7f7f7", "#fddbc7", "#ef8a62", "#b2182b"))+
+  #geom_point(data=estaciones, aes(x= Longitud, y= Latitud_De))+
+  labs(fill="Probabilidad Vibrio", title= "marea")
+  
